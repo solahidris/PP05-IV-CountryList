82bca511f3946a347046a62bfcbd78ed3753d3ee
solahidris <emailsolah@gmail.com>
2023-07-23 00:09:13 +0800

    Added Search Country UI

--- src/components/SearchCountry.js
+++ src/components/SearchCountry.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const SearchCountry = () => {
-    return (
-        <div className="bg-green-300 py-5">
-
-        </div>
-    );
+  return (
+    <div className="bg-green-300 py-5">
+      {/* <p>L-Search for a Country</p> */}
+      <input
+        type="text"
+        placeholder="🔍   Search for a country"
+        className="py-3 px-5 ml-5 rounded-md border border-stone-300 w-full"
+      />
+    </div>
+  );
 };
 
 export default SearchCountry;
