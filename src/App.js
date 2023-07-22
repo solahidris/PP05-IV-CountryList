d8375d9878856906e967e69732f600daf064253d
solahidris <emailsolah@gmail.com>
2023-07-22 17:29:11 +0800

    install tailwindcss and missing dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div>
-      <p>this is the main branch</p>
+    <div className="bg-stone-100 min-h-[100vh] p-5">
+
+      <p className="text-xs">this is the main branch</p>
+
+      <div className="container p-5 bg-stone-200 text-xs">
+        <p className="underline font-bold mb-2">code documentation</p>
+        <ol className="list-decimal list-inside">
+          <li>npm install tailwindcss</li>
+          <li>npm install @babel/plugin-proposal-private-property-in-object</li>
+          <li>-</li>
+        </ol>
+      </div>
+
     </div>
   );
 }
